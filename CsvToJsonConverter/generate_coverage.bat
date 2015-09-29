679f99c322bf41e202ee01a1a10d6d57df3b7dcd
Kyle Lewis <thekyle@gmail.com>
2015-09-29 02:53:38 -0400

    Adding SonarQube configuration and OpenCover batch file

---
+++ CsvToJsonConverter/generate_coverage.bat
@@ -0,0 +1,2 @@
+@REM @ECHO OFF
+"%LOCALAPPDATA%\Apps\OpenCover\OpenCover.Console.exe" -output:"opencover.xml" -register:user -target:"packages\NUnit.Runners.2.6.4\tools\nunit-console.exe" -targetargs:"CsvToJsonConverter.Tests\bin\Debug\CsvToJsonConverter.Tests.dll /noshadow"
