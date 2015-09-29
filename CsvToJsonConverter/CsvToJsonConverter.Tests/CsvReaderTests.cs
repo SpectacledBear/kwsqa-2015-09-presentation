ce63e36011c3856a84d7df5d59d22d27060bba68
Kyle Lewis <thekyle@gmail.com>
2015-09-29 02:53:17 -0400

    Adding tests project to generate coverage metrics

---
+++ CsvToJsonConverter/CsvToJsonConverter.Tests/CsvReaderTests.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using NUnit.Framework;
+
+namespace CsvToJsonConverter.Tests
+{
+    [TestFixture]
+    public class CsvReaderTests
+    {
+        private const int NUMBER_OF_USERS = 2;
+
+        // This is not a great test but exists to provide coverage.
+        [Test]
+        public void SomeTest()
+        {
+            string path = Path.Combine(Directory.GetCurrentDirectory(), "users.csv");
+            CsvReader reader = new CsvReader(path);
+            List<User> users = reader.LoadUsers();
+
+            Assert.IsTrue(users.Count == NUMBER_OF_USERS, "The exepected user with a specific username was not found.");
+        }
+    }
+}
